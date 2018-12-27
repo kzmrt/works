34f903dfa01e9a6c0b34a8c5c8a5bef6f42bb6ec
kzmrt <info.murata.kazuhiko@gmail.com>
2018-12-27 13:45:26 +0900

    バリデーション処理を修正。

--- pictures/forms.py
+++ pictures/forms.py
@@ -7,8 +7,8 @@ VALID_EXTENSIONS = ['.jpg']
 class UploadFileForm(forms.Form):
     image = forms.ImageField()
 
-    def clean_file(self):
-        file = self.cleaned_data['file']
-        extension = os.path.splitext(file.name)[1] # 拡張子を取得
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        extension = os.path.splitext(image.name)[1]  # 拡張子を取得
         if not extension.lower() in VALID_EXTENSIONS:
             raise forms.ValidationError('jpgファイルを選択してください！')

504a2df1b10af04ce6fa88089c2c36a90d7ce2be
kzmrt <info.murata.kazuhiko@gmail.com>
2019-12-17 10:45:03 +0900

    ID毎のテンプレート読み込み処理を追加。 カスタムタグを追加。 admin画面の表示項目を追加。

--- pictures/admin.py
+++ pictures/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from .models import Work, Image
 
-admin.site.register(Work)
-admin.site.register(Image)
+# admin.site.register(Work)
+# admin.site.register(Image)
+
+
+class WorkModelAdmin(admin.ModelAdmin):
+    # 一覧表示画面のフィールド
+    list_display = ('name', 'memo', 'author', 'created_at', 'updated_at')
+    # 一覧表示画面のソート
+    ordering = ('created_at',)  # '-created_at' とすると降順になります。
+    # 編集画面のフィールド
+    fields = ('name', 'memo', 'author')
+
+
+class ImageModelAdmin(admin.ModelAdmin):
+    # 一覧表示画面のフィールド
+    list_display = ('work', 'image', 'created_at', 'updated_at')
+    # 編集画面のフィールド
+    fields = ('work', 'image', 'data_datetime')
+
+
+admin.site.register(Work, WorkModelAdmin)
+admin.site.register(Image, ImageModelAdmin)
